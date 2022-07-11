cd50709ed9a74157bcacf3a5bbeaf72b49f77b8c
Aaron Rodi <metalhead5209@gmail.com>
2022-07-10 23:00:54 -0500

    added setState to button

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import Game from './components/game/Game';
 import Rando from './components/Rando/Rando';
+import Button from './components/Button/Button';
+import BrokenClick from './components/Brokenclick/BrokenClick';
 
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
     <div className="App">
      <Game />
      <Rando maxNum={100} />
+     <Button />
+     <BrokenClick />
     </div>
   )
 }
