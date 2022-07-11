ab832f4462a0e6aafe2041d47d02bb482ac8fc2a
Aaron Rodi <metalhead5209@gmail.com>
2022-07-10 21:59:50 -0500

    created game component, added state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-
 import './App.css'
+import Game from './components/game/Game';
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
+ return (
     <div className="App">
-     
+     <Game />
     </div>
   )
 }
